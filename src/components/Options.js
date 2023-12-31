e8d198cb34a8384bf7ac83165d63d0249e0be68d
Yafet Zerihun <zerihunyafet80@gmail.com>
2023-12-31 19:06:01 +0300

    create options component for reusablility

---
+++ src/components/Options.js
@@ -0,0 +1,15 @@
+function options({ question }) {
+  return (
+    <div>
+      <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default options;
