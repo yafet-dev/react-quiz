bdc165fd86a73e3a3d58edf4903880b03644e278
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-01-29 21:40:27 +0300

    calculate high score

--- src/Finishscreen.js
+++ src/Finishscreen.js
@@ -4,11 +4,18 @@ function Finishscreen({ points, maxPossiblePoints }) {
   let emoji;
   if (percentage === 100) emoji = "🥇";
   if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦‍♂️";
   return (
-    <p className="result">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints}
-      {Math.ceil(percentage)}%
-    </p>
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{points}</strong> out of {maxPossiblePoints}
+        {Math.ceil(percentage)}%
+      </p>
+      <p>(Highscore: X points)</p>
+    </>
   );
 }
 
