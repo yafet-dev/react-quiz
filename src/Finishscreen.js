52f193754acca3f2b17e2e10fc298092151506e7
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-01-29 20:57:36 +0300

    percentage display

--- src/Finishscreen.js
+++ src/Finishscreen.js
@@ -1,5 +1,8 @@
 function Finishscreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
   return (
     <p className="result">
       You scored <strong>{points}</strong> out of {maxPossiblePoints}
