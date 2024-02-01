4124ed090b6e4e11340dbdd74e74b01b27a91954
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-02-01 20:42:01 +0300

    add class name

--- src/Finishscreen.js
+++ src/Finishscreen.js
@@ -1,12 +1,10 @@
 function Finishscreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
   return (
-    <div>
-      <p>
-        You scored <strong>{points}</strong> out of {maxPossiblePoints}
-        {Math.ceil(percentage)}
-      </p>
-    </div>
+    <p className="result">
+      You scored <strong>{points}</strong> out of {maxPossiblePoints}
+      {Math.ceil(percentage)}%
+    </p>
   );
 }
 
