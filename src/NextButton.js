4c5d521a5f8124fd2e0ccc423b234a58526fce21
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-02-03 20:50:47 +0300

    updating the state

--- src/NextButton.js
+++ src/NextButton.js
@@ -10,6 +10,16 @@ function NextButton({ dispatch, answer, index, numQuestions }) {
         Next
       </button>
     );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
