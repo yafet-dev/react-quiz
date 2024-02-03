c7d53b0d26d4e5534183e998eb4fdbc7a34966c6
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-02-03 20:53:51 +0300

    add case

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return { ...state, status: "finished" };
     default:
       throw new Error("action unknown");
   }
