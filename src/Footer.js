af534e5e80516f6455b40bd64780072ad8332c0e
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-02-05 23:02:07 +0300

    add timer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,9 @@
-import { children } from "react";
+import { children, useEffect } from "react";
 
 function Footer({ children }) {
+  useEffect(function () {
+    setInterval(function () {}, 1000);
+  }, []);
   return <footer>{children}</footer>;
 }
 
