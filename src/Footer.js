17a62bd2500fc8c9f50d691234968a52c10bddfe
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-02-05 23:07:08 +0300

    edit footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,6 @@
 import { children, useEffect } from "react";
 
 function Footer({ children }) {
-  useEffect(function () {
-    setInterval(function () {}, 1000);
-  }, []);
   return <footer>{children}</footer>;
 }
 
