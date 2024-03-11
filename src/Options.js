e354e7026f8129126c453692156d77fa946c2dbb
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-03-11 21:55:54 +0300

    add options for the questions

--- src/Options.js
+++ src/Options.js
@@ -1,5 +1,28 @@
-function Options() {
-  return <div></div>;
+import { type } from "@testing-library/user-event/dist/type";
+
+function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={answer !== null}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Options;
